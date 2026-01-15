5b1f67d954f09f9e36c2b388862c4c97bb05e94d
Jaehyun Kim <a7420174@gmail.com>
2026-01-15 11:01:12 +0900

    Revert "BioMCP Server 사용"
    This reverts commit 4eab5f4e1c9bf2627771970a0ce8b8413e382598.

--- src/virtual_lab/constants.py
+++ src/virtual_lab/constants.py
@@ -79,11 +79,3 @@ PUBMED_TOOL_DESCRIPTION = {
         },
     },
 }
-
-BIOMCP_TOOL_DESCRIPTION = {
-            "type": "mcp",
-            "server_label": "BioMCP",
-            "server_url": "http://localhost:8000/mcp",
-            "require_approval": "never"
-        }
-        
